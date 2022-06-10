95390a98adf1697905657e3a6017573df9c6a3b4
Айтбанов Абылай <00054061@nb>
2022-06-10 11:38:02 +0600

    XOR-HashMap

--- Leetcode/1. Two Sum.py
+++ Leetcode/1. Two Sum.py
@@ -1,12 +1,13 @@
 class Solution:#HashMap O(n)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for i, value in enumerate(nums):
-            remaining = target - nums[i]
-            if remaining in seen:
-                return [i, seen[remaining]]
-            
-            seen[value] = i 
+         d={}
+         for i,value2 in enumerate(nums):
+            #value1+value2=target
+            value1=target-value2
+            if value1 in d:
+                return [d[value1],i]
+            else:
+                d[value2]=i
         # for i in range(len(nums)):
         #     for j in range(len(nums)):
         #         if i==j:
@@ -14,3 +15,4 @@ class Solution:#HashMap O(n)
         #         if nums[i]+nums[j]==target:
         #             return [i,j]
         # return []
+       
