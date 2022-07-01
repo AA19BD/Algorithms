8b6e25d7e10699d93cbfc4f6d7fa0a89a188ebab
Abilay Aytbanov <aitbanov640@gmail.com>
2022-07-01 15:33:23 +0600

    ADS-Leetcode-2022-Stack

--- ADS/Informatics/Stack, Deque, Queue/d.py
+++ ADS/Informatics/Stack, Deque, Queue/d.py
@@ -1,35 +1,26 @@
-<<<<<<< HEAD
-with open('./d.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-    sorted_list_9 = []
-    sorted_list_10 = []
-    sorted_list_11 = []
-    for i in range(len(lines)):
-        if lines[len(lines)-1] == lines[i]:
-            lines[i] += '\n'
-        if lines[i].split()[0] == '9':
-            sorted_list_9.append(lines[i])
-        if lines[i].split()[0] == '10':
-            sorted_list_10.append(lines[i] )
-        if lines[i].split()[0] == '11':
-            sorted_list_11.append(lines[i])
 
+# with open('input.txt', 'r', encoding='utf-8') as f:
+#     lines = f.readlines()
+#     sorted_list_9 = []
+#     sorted_list_10 = []
+#     sorted_list_11 = []
+#     for i in range(len(lines)):
+#         if lines[len(lines)-1] == lines[i]:
+#             lines[i] += '\n'
+#         if lines[i].split()[0] == '9':
+#             sorted_list_9.append(lines[i])
+#         if lines[i].split()[0] == '10':
+#             sorted_list_10.append(lines[i] )
+#         if lines[i].split()[0] == '11':
+#             sorted_list_11.append(lines[i])
+#
+#
+#     sum_of_sorted_lists = sorted_list_9 + sorted_list_10 + sorted_list_11
+#     print(*sum_of_sorted_lists, sep = '')
+#
+#     f.close()
 
-    sum_of_sorted_lists = sorted_list_9 + sorted_list_10 + sorted_list_11
-    print(*sum_of_sorted_lists, sep = '')
-
-
-
-
-
-
-
-
-
-
-    f.close()
-=======
-f = open('d.txt','r')
+f = open('input.txt', 'r')
 d = dict()
 
 list_of_9 = []
@@ -50,9 +41,9 @@ sum_of_lists = list_of_9 + list_of_10 + list_of_11
 for i in sum_of_lists:
     print(i)
 
-# with open('d.txt', 'w') as f:
-#     for i in sum_of_lists:
-#         print(i, end='\n',file=f)
+with open('input.txt', 'w') as f:
+    for i in sum_of_lists:
+        print(i, end='\n',file=f)
 #
 f.close()
->>>>>>> 675f5f5296ef812815d41748a2bbe08c7eb58d52
+
