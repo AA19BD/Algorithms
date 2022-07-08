99b7bf13d0ab8b8aa32af534c31dcbc15e62479b
Abilay Aytbanov <aitbanov640@gmail.com>
2022-07-08 10:13:10 +0600

    Added_Linked_list_InsertAfter_PushAtFront-Append-Display

--- ADS/week2/Set2(LinkedList).py
+++ ADS/week2/Set2(LinkedList).py
@@ -8,17 +8,22 @@ class LinkedList:
     def __init__(self):
         self.head = None # None()
 
-    def append(self, data):
+    def append(self, data): #Push at the end
         new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
         cur = self.head
-        while cur.next != None:
+        while (cur.next):
             cur = cur.next
         cur.next = new_node
 
     def length(self):
         total = 0
         cur = self.head
-        while cur.next != None:
+        while (cur.next):
             cur = cur.next
             total += 1
         return total
@@ -26,7 +31,7 @@ class LinkedList:
     def display(self):
         elems = []
         cur = self.head
-        while cur.next != None:
+        while (cur.next):
             cur = cur.next
             elems.append(cur.data)
         return elems
@@ -35,6 +40,7 @@ class LinkedList:
         if index >= self.length():
             raise IndexError
             return None
+
         cur_node = self.head
         cur_idx = 0
         while True:
@@ -43,14 +49,53 @@ class LinkedList:
                 return cur_node.data
             cur_idx += 1
 
+    def push(self, data): #Push at front
+        # 1 & 2: Allocate the Node &
+        #        Put in the data
+        new_node = Node(data)
+        # 3. Make next of new Node as head
+        new_node.next = self.head
 
-llist = LinkedList()
-llist.head = Node(0)
-llist.append(1)
-llist.append(2)
-llist.append(3)
+        # 4. Move the head to point to new Node
+        self.head = new_node
 
-print(llist.head.next.data)
-print(llist.length())
-print(llist.display())
-print(f"Cur_node.data == {llist.get_index(0)}")
+    def insertAfter(self, prev_node, data):
+        #1 Check if prev_node exists
+        if prev_node is None:
+            print('The prev node must in LinkedList')
+            return
+        #2 Create new node and put data in
+        new_node = Node(data)
+        #3 Make next of new Node as next of prev_node
+        new_node.next = prev_node.next
+        # make next of prev_node as new_node
+        prev_node.next = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+
+if __name__=='__main__':
+    llist = LinkedList()
+    # Insert 6.  So linked list becomes 6->None
+    llist.append(6)
+
+    # Insert 7 at the beginning. So linked list becomes 7->6->None
+    llist.push(7)
+
+    # Insert 1 at the beginning. So linked list becomes 1->7->6->None
+    llist.push(1)
+
+    # Insert 4 at the end. So linked list becomes 1->7->6->4->None
+    llist.append(4)
+
+    # Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None
+    llist.insertAfter(llist.head.next, 8)
+
+    print(llist.head.next.data)
+    print(llist.length())
+    # print(llist.display())
+    print(llist.printList())
+    print(f"Cur_node.data == {llist.get_index(0)}")
