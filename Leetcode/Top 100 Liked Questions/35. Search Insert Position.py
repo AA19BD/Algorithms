9e3c7f004e2c21114f579c8a2ebe322527a2e209
Abilay Aytbanov <aitbanov640@gmail.com>
2022-07-21 11:08:11 +0600

    Udacity-Stack-LinkedList-InsertFirst-DeleteFirst

--- Leetcode/Top 100 Liked Questions/35. Search Insert Position.py
+++ Leetcode/Top 100 Liked Questions/35. Search Insert Position.py
@@ -1,14 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        l,r=0,len(nums)-1
-        while l<=r:
-            mid=l+(r-l)//2
-            if nums[mid]==target:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r-l)//2
+            if nums[mid] == target:
                 return mid
-            elif nums[mid]<target:
-                l=mid+1
+            elif nums[mid] < target:
+                l = mid + 1
             else:
-                r=mid-1
+                r = mid - 1
         
 
         return l
